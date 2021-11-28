59ac9fa24eaddc7961e0d5273e5e12b1d0c8cc1d
Holappa NiinaE <niina.p.puurunen@gmail.com>
2021-11-28 22:33:43 +0200

    Sorted questions in an array and added id's to html

--- tietovisa/quiz/tietovisa.js
+++ tietovisa/quiz/tietovisa.js
@@ -1,5 +1,65 @@
-function f1() {
+/*function f1() {
     let name = document.getElementById("formGroupExampleInput").value;
-    document.getElementById("text").innerHTML = "";
+    document.getElementById("quiz").innerHTML = "";
     
-}
+}*/
+
+const QUIZ_CONTAINER = document.getElementById("quiz");
+const SUBMIT_BUTTON = document.getElementById("submit");
+
+const QUESTIONS = [
+    {
+        question = "Kuinka monta maanosaa on?",
+        answers: {
+            a: "5",
+            b: "7",
+            c: "10",
+            d: "4"
+        },
+        correctAnswer = "b"
+    },
+
+    {
+        question = "Minkä maan pääkaupunki Madrid on?",
+        answers: {
+            a: "Ruotsin", 
+            b: "Saksan",
+            c: "Espanjan",
+            d: "Ranskan"
+        },
+        correctAnswer = "c"
+    },
+
+    { 
+      question = "Kuinka monessa päivässä maa kierää auringon?",
+      answers: {
+        a: "200:ssa",
+        b: "90:ssä",
+        c: "150:ssa",
+        d: "365:ssä"
+      },
+      correctAnswer = "d"
+    },
+
+    {
+        question = "Mikä maa on pinta-alaltaan maailman pienin valtio?",
+        answers = {
+            a: "Vatikaani",
+            b: "Islanti",
+            c: "Monaco",
+            d: "Portugali"
+        },
+        correctAnswer = "a"
+    },
+
+    {
+        question = "Mikä maa on pinta-alaltaan maailman suurin valtio?",
+        answers = {
+            a: "Yhdysvallat",
+            b: "Kanada",
+            c: "Kiina",
+            d: "Venäjä"
+        },
+        correctAnswer = "d"
+    },
+]
