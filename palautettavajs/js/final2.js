110f99eda8f07965a436128028c70996d2c327fb
Holappa NiinaE <niina.p.puurunen@gmail.com>
2021-12-09 13:01:50 +0200

    added comments to js

--- palautettavajs/js/final2.js
+++ palautettavajs/js/final2.js
@@ -1,10 +1,12 @@
+// Lisätään kuuntelija painikkeelle
 document.querySelector("button").addEventListener("click", addContent);
 
+// Tallennetaan tiedot muuttujiin
 let body = document.querySelector("body");
 let operator = document.getElementById("fullName");
 let message = document.getElementById("textArea");
 
-
+// Luodaan funktio addContent 
 function addContent() {
     let h3 = document.createElement("h3");
     h3.textContent = operator.value + " " + new Date().toLocaleString();
